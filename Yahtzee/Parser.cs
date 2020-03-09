f321306ae888db04d8f759fb48bcf225e038394a
James-Kehoe-MYOB <james.kehoe@myob.com>
2020-03-10 09:33:35 +1100

    moved game logic into GameHandler class

--- Yahtzee/Parser.cs
+++ Yahtzee/Parser.cs
@@ -5,7 +5,7 @@ using System.Text.RegularExpressions;
 namespace Yahtzee {
     public static class Parser {
         public static bool RollParse(string input) {
-            var expr = "(([1-6]|-),([1-6]|-),([1-6]|-),([1-6]|-),([1-6]|-))";
+            var expr = "^(([1-6]|-),([1-6]|-),([1-6]|-),([1-6]|-),([1-6]|-))$";
             if (Regex.IsMatch(input, expr)) {
                 return true;
             }
